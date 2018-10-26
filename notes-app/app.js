1de04ab2a107353e5f620be2025ec5be9e3eef4c
Andrew Mead <andrewjosephmead@gmail.com>
2018-10-26 15:12:28 -0400

    Lesson: Importing Your Own Files

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,5 @@
-const fs = require('fs')
+const getNotes = require('./notes.js')
 
-// fs.writeFileSync('notes.txt', 'My name is Andrew.')
+const msg = getNotes()
 
-fs.appendFileSync('notes.txt', ' I live in Philadelphia.')
+console.log(msg)
---
+++ notes-app/notes.js
@@ -0,0 +1,5 @@
+const getNotes = function () {
+    return 'Your notes...'
+}
+
+module.exports = getNotes
