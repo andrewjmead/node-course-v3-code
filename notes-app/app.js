677a932bc205e5b586b744c41e4811e656adb665
Andrew Mead <andrewjosephmead@gmail.com>
2018-10-29 10:34:31 -0400

    Lesson: Printing in Color

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,8 @@
-const validator = require('validator')
+const chalk = require('chalk')
 const getNotes = require('./notes.js')
 
 const msg = getNotes()
 console.log(msg)
 
-console.log(validator.isURL('https/mead.io'))
+const greenMsg = chalk.green.inverse.bold('Success!')
+console.log(greenMsg)
