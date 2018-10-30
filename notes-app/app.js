eee457ba12836008b594897706d55e56b9d62519
Andrew Mead <andrewjosephmead@gmail.com>
2018-10-30 09:56:43 -0400

    Lesson: Getting Input from User

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,12 @@
 const chalk = require('chalk')
 const getNotes = require('./notes.js')
 
-const msg = getNotes()
-console.log(msg)
+const command = process.argv[2]
 
-const greenMsg = chalk.blue.inverse.bold('Success!')
-console.log(greenMsg)
+console.log(process.argv)
+
+if (command === 'add') {
+    console.log('Adding note!')
+} else if (command === 'remove') {
+    console.log('Removing note!')
+}
