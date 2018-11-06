8b64eaf3757e7aa6d1a18a8b46094f63ff84d88f
Andrew Mead <andrewjosephmead@gmail.com>
2018-11-06 10:46:35 -0500

    Lesson: Asynchronous Basics

---
+++ weather-app/app.js
@@ -0,0 +1,11 @@
+console.log('Starting')
+
+setTimeout(() => {
+    console.log('2 Second Timer')
+}, 2000)
+
+setTimeout(() => {
+    console.log('0 Second Timer')
+}, 0)
+
+console.log('Stopping')
