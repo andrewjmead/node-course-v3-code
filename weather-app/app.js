135ed618396ec3fd6843a64efbb344c3c40d37fb
Andrew Mead <andrewjosephmead@gmail.com>
2018-11-07 10:31:46 -0500

    Lesson: Making HTTP Requests

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,11 +1,8 @@
-console.log('Starting')
+const request = require('request')
 
-setTimeout(() => {
-    console.log('2 Second Timer')
-}, 2000)
+const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/37.8267,-122.4233'
 
-setTimeout(() => {
-    console.log('0 Second Timer')
-}, 0)
-
-console.log('Stopping')
+request({ url: url }, (error, response) => {
+    const data = JSON.parse(response.body)
+    console.log(data.currently)
+})
