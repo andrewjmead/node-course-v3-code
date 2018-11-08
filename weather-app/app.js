833f77cdc85ad417a5e6403fedece6645b8028d2
Andrew Mead <andrewjosephmead@gmail.com>
2018-11-08 10:00:40 -0500

    Lesson: An HTTP Request Challenge

--- weather-app/app.js
+++ weather-app/app.js
@@ -5,3 +5,14 @@ const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/3
 request({ url: url, json: true }, (error, response) => {
     console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degress out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
 })
+
+// Geocoding
+// Address -> Lat/Long -> Weather
+
+const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
+
+request({ url: geocodeURL, json: true }, (error, response) => {
+    const latitude = response.body.features[0].center[0]
+    const longitude = response.body.features[0].center[1]
+    console.log(latitude, longitude)
+})
