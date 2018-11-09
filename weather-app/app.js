d6f02da793e9bf3f0aaaf627c0da89d9f4fe6bca
Andrew Mead <andrewjosephmead@gmail.com>
2018-11-09 14:25:51 -0500

    Lesson: Callback Abstraction Challenge

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,35 +1,12 @@
-const request = require('request')
 const geocode = require('./utils/geocode')
-
-// const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/37.8267,-112'
-
-// request({ url: url, json: true }, (error, response) => {
-//     if (error) {
-//         console.log('Unable to connect to weather service!')
-//     } else if (response.body.error) {
-//         console.log('Unable to find location')
-//     } else {
-//         console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degress out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')   
-//     }
-// })
-
-// const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/philadelphia.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
-
-// request({ url: geocodeURL, json: true }, (error, response) => {
-//     if (error) {
-//         console.log('Unable to connect to location services!')
-//     } else if (response.body.features.length === 0) {
-//         console.log('Unable to find location. Try another search.')
-//     } else {
-//         const latitude = response.body.features[0].center[0]
-//         const longitude = response.body.features[0].center[1]
-//         console.log(latitude, longitude)
-//     }
-// })
-
-
+const forecast = require('./utils/forecast')
 
 geocode('Boston', (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
+})
+
+forecast(-75.7088, 44.1545, (error, data) => {
     console.log('Error', error)
     console.log('Data', data)
 })
---
+++ weather-app/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
+
+    request({ url: url, json: true }, (error, response) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (response.body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degress out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
+        }
+    })
+}
+
+module.exports = forecast
