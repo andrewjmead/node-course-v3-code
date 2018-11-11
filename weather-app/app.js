c20ac9fac9a4bb728e2213603fcab40179bc48f5
Andrew Mead <andrewjosephmead@gmail.com>
2018-11-11 10:25:13 -0500

    Lesson: Callback Chaining

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,12 +1,23 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-geocode('Boston', (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-})
+const address = process.argv[2]
 
-forecast(-75.7088, 44.1545, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-})
+if (!address) {
+    console.log('Please provide an address')
+} else {
+    geocode(address, (error, data) => {
+        if (error) {
+            return console.log(error)
+        }
+
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error)
+            }
+
+            console.log(data.location)
+            console.log(forecastData)
+        })
+    })
+}
--- weather-app/utils/geocode.js
+++ weather-app/utils/geocode.js
@@ -10,8 +10,8 @@ const geocode = (address, callback) => {
             callback('Unable to find location. Try another search.', undefined)
         } else {
             callback(undefined, {
-                latitude: response.body.features[0].center[0],
-                longitude: response.body.features[0].center[1],
+                latitude: response.body.features[0].center[1],
+                longitude: response.body.features[0].center[0],
                 location: response.body.features[0].place_name
             })
         }
