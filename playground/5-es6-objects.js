e0abfa01f039481b0121df9fa1e3424ee4264213
Andrew Mead <andrewjosephmead@gmail.com>
2018-11-13 09:41:06 -0500

    Lesson: ES6 Aside: Object Property Shorthand and Destructuring

---
+++ playground/5-es6-objects.js
@@ -0,0 +1,36 @@
+// Object property shorthand
+
+const name = 'Andrew'
+const userAge = 27
+
+const user = {
+    name,
+    age: userAge,
+    location: 'Philadelphia'
+}
+
+console.log(user)
+
+// Object destructuring
+
+const product = {
+    label: 'Red notebook',
+    price: 3,
+    stock: 201,
+    salePrice: undefined,
+    rating: 4.2
+}
+
+// const label = product.label
+// const stock = product.stock
+
+// const {label:productLabel, stock, rating = 5} = product
+// console.log(productLabel)
+// console.log(stock)
+// console.log(rating)
+
+const transaction = (type, { label, stock }) => {
+    console.log(type, label, stock)
+}
+
+transaction('order', product)
