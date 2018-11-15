a02958a4dfeebbb27915304a92ba66a6773411b6
Andrew Mead <andrewjosephmead@gmail.com>
2018-11-15 09:56:23 -0500

    Lesson: Hello Express!

---
+++ web-server/src/app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+
+const app = express()
+
+app.get('', (req, res) => {
+    res.send('Hello express!')
+})
+
+app.get('/help', (req, res) => {
+    res.send('Help page')
+})
+
+app.get('/about', (req, res) => {
+    res.send('About')
+})
+
+app.get('/weather', (req, res) => {
+    res.send('Your weather')
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
+})
