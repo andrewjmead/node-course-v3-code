f638cd24602a4a7bba3aba8bb90e24ab02e68e48
Andrew Mead <andrewjosephmead@gmail.com>
2018-11-15 11:19:38 -0500

    Lesson: Serving up Static Assets

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,22 +1,10 @@
+const path = require('path')
 const express = require('express')
 
 const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
 
-app.get('', (req, res) => {
-    res.send('<h1>Weather</h1>')
-})
-
-app.get('/help', (req, res) => {
-    res.send([{
-        name: 'Andrew'
-    }, {
-        name: 'Sarah'
-    }])
-})
-
-app.get('/about', (req, res) => {
-    res.send('<h1>About</h1>')
-})
+app.use(express.static(publicDirectoryPath))
 
 app.get('/weather', (req, res) => {
     res.send({
