4541ac36b057f98c644924d3b52d4732694bfd76
Andrew Mead <andrewjosephmead@gmail.com>
2018-11-23 11:59:57 -0500

    Lesson: The Query String

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -40,9 +40,29 @@ app.get('/help', (req, res) => {
 })
 
 app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an address!'
+        })
+    }
+
     res.send({
         forecast: 'It is snowing',
-        location: 'Philadelphia'
+        location: 'Philadelphia',
+        address: req.query.address
+    })
+})
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+
+    console.log(req.query.search)
+    res.send({
+        products: []
     })
 })
 
