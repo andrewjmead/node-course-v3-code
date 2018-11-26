5120d77f91aa2f4c89e9218b4de29b4184541dde
Andrew Mead <andrewjosephmead@gmail.com>
2018-11-26 11:19:18 -0500

    Lesson: Browser HTTP Requests with Fetch

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,12 @@
 console.log('Client side javascript file is loaded!')
+
+fetch('http://localhost:3000/weather?address=!').then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            console.log(data.error)
+        } else {
+            console.log(data.location)
+            console.log(data.forecast)
+        }
+    })
+})
