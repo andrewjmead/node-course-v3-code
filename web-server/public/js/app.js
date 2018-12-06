0a90167e5e97f3fecec0515cf68ff9f4b4349a02
Andrew Mead <andrewjosephmead@gmail.com>
2018-12-06 10:08:52 -0500

    Lesson: Integrating Git

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client side javascript file is loaded!')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
