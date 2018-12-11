5bd2950cb6d926adab2b6b1800e483795e119859
Andrew Mead <andrewjosephmead@gmail.com>
2018-12-11 11:06:38 -0500

    Lesson: Connecting and Inserting Documents

---
+++ task-manager/mongodb.js
@@ -0,0 +1,20 @@
+// CRUD create read update delete
+
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+    if (error) {
+        return console.log('Unable to connect to database!')
+    }
+
+    const db = client.db(databaseName)
+    
+    db.collection('users').insertOne({
+        name: 'Andrew',
+        age: 27
+    })
+})
