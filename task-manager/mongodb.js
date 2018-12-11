e8cdc1775b341d43c7925b7d910d61568b2230e7
Andrew Mead <andrewjosephmead@gmail.com>
2018-12-11 11:58:27 -0500

    Lesson: Inserting Documents

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -13,8 +13,49 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
     const db = client.db(databaseName)
     
-    db.collection('users').insertOne({
-        name: 'Andrew',
-        age: 27
+    // db.collection('users').insertOne({
+    //     name: 'Andrew',
+    //     age: 27
+    // }, (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert user')
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Jen',
+    //         age: 28
+    //     }, {
+    //         name: 'Gunther',
+    //         age: 27
+    //     }
+    // ], (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert documents!')
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+    db.collection('tasks').insertMany([
+        {
+            description: 'Clean the house',
+            completed: true
+        },{
+            description: 'Renew inspection',
+            completed: false
+        },{
+            description: 'Pot plants',
+            completed: false
+        }
+    ], (error, result) => {
+        if (error) {
+            return console.log('Unable to insert tasks!')
+        }
+
+        console.log(result.ops)
     })
 })
