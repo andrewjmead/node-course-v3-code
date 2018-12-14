26b111b16917bead4d0d6122cf4fd7a8c1fa4f28
Andrew Mead <andrewjosephmead@gmail.com>
2018-12-14 10:32:48 -0500

    Lesson: Creating a Mongoose Model

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -14,13 +14,33 @@ const User = mongoose.model('User', {
     }
 })
 
-const me = new User({
-    name: 'Andrew',
-    age: 'Mike'
+// const me = new User({
+//     name: 'Andrew',
+//     age: 'Mike'
+// })
+
+// me.save().then(() => {
+//     console.log(me)
+// }).catch((error) => {
+//     console.log('Error!', error)
+// })
+
+const Task = mongoose.model('Task', {
+    description: {
+        type: String
+    },
+    completed: {
+        type: Boolean
+    }
 })
 
-me.save().then(() => {
-    console.log(me)
+const task = new Task({
+    description: 'Learn the Mongoose library',
+    completed: false
+})
+
+task.save().then(() => {
+    console.log(task)
 }).catch((error) => {
-    console.log('Error!', error)
+    console.log(error)
 })
