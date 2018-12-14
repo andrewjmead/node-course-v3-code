6be5dbe00b4975625e388db7e4bfd0d233d52216
Andrew Mead <andrewjosephmead@gmail.com>
2018-12-14 12:51:52 -0500

    Lesson: Data Validation and Sanitization: Part II

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -23,6 +23,17 @@ const User = mongoose.model('User', {
             }
         }
     },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7,
+        trim: true,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Password cannot contain "password"')
+            }
+        }
+    },
     age: {
         type: Number,
         default: 0,
@@ -34,33 +45,36 @@ const User = mongoose.model('User', {
     }
 })
 
-const me = new User({
-    name: '   Andrew  ',
-    email: 'MYEMAIL@MEAD.IO   '
-})
+// const me = new User({
+//     name: '   Andrew  ',
+//     email: 'MYEMAIL@MEAD.IO   ',
+//     password: 'phone098!'
+// })
 
-me.save().then(() => {
-    console.log(me)
-}).catch((error) => {
-    console.log('Error!', error)
-})
+// me.save().then(() => {
+//     console.log(me)
+// }).catch((error) => {
+//     console.log('Error!', error)
+// })
 
 const Task = mongoose.model('Task', {
     description: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     }
 })
 
-// const task = new Task({
-//     description: 'Learn the Mongoose library',
-//     completed: false
-// })
+const task = new Task({
+    description: '  Eat lunch'
+})
 
-// task.save().then(() => {
-//     console.log(task)
-// }).catch((error) => {
-//     console.log(error)
-// })
+task.save().then(() => {
+    console.log(task)
+}).catch((error) => {
+    console.log(error)
+})
