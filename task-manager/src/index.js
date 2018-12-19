72015c24b7e51bd716f3678cc3af219df0921e06
Andrew Mead <andrewjosephmead@gmail.com>
2018-12-19 11:31:20 -0500

    Lesson: Resource Reading Endpoints: Part I

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -18,6 +18,28 @@ app.post('/users', (req, res) => {
     })
 })
 
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+
+    User.findById(_id).then((user) => {
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
 app.post('/tasks', (req, res) => {
     const task = new Task(req.body)
 
