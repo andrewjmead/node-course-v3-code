679710a8ef1c8d1be1bc6f608351045a5b25af0f
Andrew Mead <andrewjosephmead@gmail.com>
2018-12-26 10:28:29 -0500

    Lesson: Resource Updating Endpoints: Part II

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -102,6 +102,28 @@ app.get('/tasks/:id', async (req, res) => {
     }
 })
 
+app.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description', 'completed']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' })
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true})
+
+        if (!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
