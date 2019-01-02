290572d9221bb6b0b0380d2acd3fa13df5497b29
Andrew Mead <andrewjosephmead@gmail.com>
2019-01-02 10:10:29 -0500

    Lesson: Express Middleware

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -6,6 +6,18 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
+// app.use((req, res, next) => {
+//     if (req.method === 'GET') {
+//         res.send('GET requests are disabled')
+//     } else {
+//         next()
+//     }
+// })
+
+app.use((req, res, next) => {
+    res.status(503).send('Site is currently down. Check back soon!')
+})
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
