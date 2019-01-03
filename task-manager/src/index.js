d45e2a3c2a0a4fda8910f72db018dd13f257575e
Andrew Mead <andrewjosephmead@gmail.com>
2019-01-03 10:32:15 -0500

    Lesson: Hiding Private Data

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -25,15 +25,3 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
-
-const jwt = require('jsonwebtoken')
-
-const myFunction = async () => {
-    const token = jwt.sign({ _id: 'abc123' }, 'thisismynewcourse', { expiresIn: '7 days' })
-    console.log(token)
-
-    const data = jwt.verify(token, 'thisismynewcourse')
-    console.log(data)
-}
-
-myFunction()
--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -49,6 +49,16 @@ const userSchema = new mongoose.Schema({
     }]
 })
 
+userSchema.methods.toJSON = function () {
+    const user = this
+    const userObject = user.toObject()
+
+    delete userObject.password
+    delete userObject.tokens
+
+    return userObject
+}
+
 userSchema.methods.generateAuthToken = async function () {
     const user = this
     const token = jwt.sign({ _id: user._id.toString() }, 'thisismynewcourse')
