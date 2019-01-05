1776e1ef2e485b71a5b402765e5ae372eea217d7
Andrew Mead <andrewjosephmead@gmail.com>
2019-01-05 10:03:21 -0500

    Lesson: Filtering Data

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -17,9 +17,19 @@ router.post('/tasks', auth, async (req, res) => {
     }
 })
 
+// GET /tasks?completed=true
 router.get('/tasks', auth, async (req, res) => {
+    const match = {}
+
+    if (req.query.completed) {
+        match.completed = req.query.completed === 'true'
+    }
+
     try {
-        await req.user.populate('tasks').execPopulate()
+        await req.user.populate({
+            path: 'tasks',
+            match
+        }).execPopulate()
         res.send(req.user.tasks)
     } catch (e) {
         res.status(500).send()
