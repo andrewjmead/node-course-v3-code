74e5f84c8c86ffb0269260a9fb2584cc427b48c4
Andrew Mead <andrewjosephmead@gmail.com>
2019-01-08 11:56:30 -0500

    Lesson: Validating File Uploads

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -9,7 +9,17 @@ const port = process.env.PORT || 3000
 
 const multer = require('multer')
 const upload = multer({
-    dest: 'images'
+    dest: 'images',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(doc|docx)$/)) {
+            return cb(new Error('Please upload a Word document'))
+        }
+
+        cb(undefined, true)
+    }
 })
 app.post('/upload', upload.single('upload'), (req, res) => {
     res.send()
