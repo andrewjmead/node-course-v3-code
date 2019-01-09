17364aefe8f27258a572453ed16179b8b59a5024
Andrew Mead <andrewjosephmead@gmail.com>
2019-01-09 09:55:36 -0500

    Lesson: Serving up Files

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -107,4 +107,19 @@ router.delete('/users/me/avatar', auth, async (req, res) => {
     res.send()
 })
 
+router.get('/users/:id/avatar', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+
+        if (!user || !user.avatar) {
+            throw new Error()
+        }
+
+        res.set('Content-Type', 'image/jpg')
+        res.send(user.avatar)
+    } catch (e) {
+        res.status(404).send()
+    }
+})
+
 module.exports = router
