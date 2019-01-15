b73b9e6b203f9859541c09ee7df1bbad42b6f310
Andrew Mead <andrewjosephmead@gmail.com>
2019-01-15 10:29:18 -0500

    Lesson: Jest Testing Framework

---
+++ task-manager/tests/math.test.js
@@ -0,0 +1,18 @@
+test('Hello world!', () => {
+
+})
+
+test('This should fail', () => {
+    throw new Error('Failure!')
+})
+
+// 
+// Why test?
+// 
+// - Saves time
+// - Creates reliable software
+// - Gives flexibility to developers
+//   - Refactoring
+//   - Collaborating
+//   - Profiling
+// - Peace of mind
