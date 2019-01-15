b5e9319bfd5280e7a8911c8889205a5dc24753ec
Andrew Mead <andrewjosephmead@gmail.com>
2019-01-15 12:08:47 -0500

    Lesson: Writing Your Own Tests

--- task-manager/src/math.js
+++ task-manager/src/math.js
@@ -1,5 +1,15 @@
 const calculateTip = (total, tipPercent = .25) => total + (total * tipPercent)
 
+const fahrenheitToCelsius = (temp) => {
+    return (temp - 32) / 1.8
+}
+
+const celsiusToFahrenheit = (temp) => {
+    return (temp * 1.8) + 32
+}
+
 module.exports = {
-    calculateTip
+    calculateTip,
+    fahrenheitToCelsius,
+    celsiusToFahrenheit
 }
--- task-manager/tests/math.test.js
+++ task-manager/tests/math.test.js
@@ -1,4 +1,4 @@
-const { calculateTip } = require('../src/math')
+const { calculateTip, celsiusToFahrenheit, fahrenheitToCelsius } = require('../src/math')
 
 test('Should calculate total with tip', () => {
     const total = calculateTip(10, .3)
@@ -10,13 +10,12 @@ test('Should calculate total with default tip', () => {
     expect(total).toBe(12.5)
 })
 
-// 
-// Why test?
-// 
-// - Saves time
-// - Creates reliable software
-// - Gives flexibility to developers
-//   - Refactoring
-//   - Collaborating
-//   - Profiling
-// - Peace of mind
+test('Should convert 32 F to 0 C', () => {
+    const temp = fahrenheitToCelsius(32)
+    expect(temp).toBe(0)
+})
+
+test('Should convert 0 C to 32 F', () => {
+    const temp = celsiusToFahrenheit(0)
+    expect(temp).toBe(32)
+})
