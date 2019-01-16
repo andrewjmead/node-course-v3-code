1d32007ba5eeeafb6a75ecd5f2f2e8621f9466de
Andrew Mead <andrewjosephmead@gmail.com>
2019-01-16 10:15:48 -0500

    Lesson: Testing Asynchronous Code

--- task-manager/src/math.js
+++ task-manager/src/math.js
@@ -8,8 +8,21 @@ const celsiusToFahrenheit = (temp) => {
     return (temp * 1.8) + 32
 }
 
+const add = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (a < 0 || b < 0) {
+                return reject('Numbers must be non-negative')
+            }
+
+            resolve(a + b)
+        }, 2000)
+    })
+}
+
 module.exports = {
     calculateTip,
     fahrenheitToCelsius,
-    celsiusToFahrenheit
+    celsiusToFahrenheit,
+    add
 }
--- task-manager/tests/math.test.js
+++ task-manager/tests/math.test.js
@@ -1,4 +1,4 @@
-const { calculateTip, celsiusToFahrenheit, fahrenheitToCelsius } = require('../src/math')
+const { calculateTip, celsiusToFahrenheit, fahrenheitToCelsius, add } = require('../src/math')
 
 test('Should calculate total with tip', () => {
     const total = calculateTip(10, .3)
@@ -18,4 +18,23 @@ test('Should convert 32 F to 0 C', () => {
 test('Should convert 0 C to 32 F', () => {
     const temp = celsiusToFahrenheit(0)
     expect(temp).toBe(32)
+})
+
+// test('Async test demo', (done) => {
+//     setTimeout(() => {
+//         expect(1).toBe(2)
+//         done()
+//     }, 2000)
+// })
+
+test('Should add two numbers', (done) => {
+    add(2, 3).then((sum) => {
+        expect(sum).toBe(5)
+        done()
+    })
+})
+
+test('Should add two numbers async/await', async () => {
+    const sum = await add(10, 22)
+    expect(sum).toBe(32)  
 })
