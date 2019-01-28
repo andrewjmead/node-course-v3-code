a4b54c33d8af87423958e2d3acf353c534d8b419
Andrew Mead <andrewjosephmead@gmail.com>
2019-01-28 10:05:47 -0500

    Lesson: Getting Started with Socket.io

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,13 +1,21 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
 
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
 const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-app.listen(port, () => {
+io.on('connection', () => {
+    console.log('New WebSocket connection')
+})
+
+server.listen(port, () => {
     console.log(`Server is up on port ${port}!`)
 })
