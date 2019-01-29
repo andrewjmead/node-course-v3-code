6d10662807aae5cf01ef5e7f60985f96a0bf32d1
Andrew Mead <andrewjosephmead@gmail.com>
2019-01-29 09:33:25 -0500

    Lesson: Socket.io Events

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,1 +1,10 @@
-io()
+const socket = io()
+
+socket.on('countUpdated', (count) => {
+    console.log('The count has been updated!', count)
+})
+
+document.querySelector('#increment').addEventListener('click', () => {
+    console.log('Clicked')
+    socket.emit('increment')
+})
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -12,8 +12,17 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-io.on('connection', () => {
+let count = 0
+
+io.on('connection', (socket) => {
     console.log('New WebSocket connection')
+
+    socket.emit('countUpdated', count)
+
+    socket.on('increment', () => {
+        count++
+        io.emit('countUpdated', count)
+    })
 })
 
 server.listen(port, () => {
