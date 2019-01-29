02983596d81fb6011c9864658be6b1df831a5808
Andrew Mead <andrewjosephmead@gmail.com>
2019-01-29 11:20:13 -0500

    Lesson: Broadcasting Events

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -16,9 +16,14 @@ io.on('connection', (socket) => {
     console.log('New WebSocket connection')
 
     socket.emit('message', 'Welcome!')
+    socket.broadcast.emit('message', 'A new user has joined!')
 
     socket.on('sendMessage', (message) => {
         io.emit('message', message)
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left!')
     })
 })
 
