2dac84579865e96c635a5a800ab082cc4a531ab9
Andrew Mead <andrewjosephmead@gmail.com>
2019-01-29 10:31:17 -0500

    Lesson: Socket.io Events Challenge

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,10 +1,13 @@
 const socket = io()
 
-socket.on('countUpdated', (count) => {
-    console.log('The count has been updated!', count)
+socket.on('message', (message) => {
+    console.log(message)
 })
 
-document.querySelector('#increment').addEventListener('click', () => {
-    console.log('Clicked')
-    socket.emit('increment')
+document.querySelector('#message-form').addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const message = e.target.elements.message.value
+
+    socket.emit('sendMessage', message)
 })
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -12,16 +12,13 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-let count = 0
-
 io.on('connection', (socket) => {
     console.log('New WebSocket connection')
 
-    socket.emit('countUpdated', count)
+    socket.emit('message', 'Welcome!')
 
-    socket.on('increment', () => {
-        count++
-        io.emit('countUpdated', count)
+    socket.on('sendMessage', (message) => {
+        io.emit('message', message)
     })
 })
 
