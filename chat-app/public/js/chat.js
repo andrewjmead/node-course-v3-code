6ded7295217d96dc0d22b1d64ae71cab978752a8
Andrew Mead <andrewjosephmead@gmail.com>
2019-01-30 10:20:55 -0500

    Lesson: Sharing Location

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -11,3 +11,16 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
 
     socket.emit('sendMessage', message)
 })
+
+document.querySelector('#send-location').addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser.')
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        })
+    })
+})
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -22,6 +22,10 @@ io.on('connection', (socket) => {
         io.emit('message', message)
     })
 
+    socket.on('sendLocation', (coords) => {
+        io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+    })
+
     socket.on('disconnect', () => {
         io.emit('message', 'A user has left!')
     })
