adf53d321ee8a09c2ba9d547e08dbe5f2ca5a182
Andrew Mead <andrewjosephmead@gmail.com>
2019-01-31 09:57:29 -0500

    Lesson: Rendering Messages

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -5,9 +5,17 @@ const $messageForm = document.querySelector('#message-form')
 const $messageFormInput = $messageForm.querySelector('input')
 const $messageFormButton = $messageForm.querySelector('button')
 const $sendLocationButton = document.querySelector('#send-location')
+const $messages = document.querySelector('#messages')
+
+// Templates
+const messageTemplate = document.querySelector('#message-template').innerHTML
 
 socket.on('message', (message) => {
     console.log(message)
+    const html = Mustache.render(messageTemplate, {
+        message
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
 })
 
 $messageForm.addEventListener('submit', (e) => {
