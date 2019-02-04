a164843ef9098c9c6cb54dba5fb30e24f71ef879
Andrew Mead <andrewjosephmead@gmail.com>
2019-02-04 12:02:06 -0500

    Lesson: Storing Users: Part II

--- chat-app/src/utils/users.js
+++ chat-app/src/utils/users.js
@@ -1,7 +1,5 @@
 const users = []
 
-// addUser, removeUser, getUser, getUsersInRoom
-
 const addUser = ({ id, username, room }) => {
     // Clean the data
     username = username.trim().toLowerCase()
@@ -40,15 +38,18 @@ const removeUser = (id) => {
     }
 }
 
-addUser({
-    id: 22,
-    username: 'Andrew  ',
-    room: '  South Philly'
-})
+const getUser = (id) => {
+    return users.find((user) => user.id === id)
+}
 
-console.log(users)
+const getUsersInRoom = (room) => {
+    room = room.trim().toLowerCase()
+    return users.filter((user) => user.room === room)
+}
 
-const removedUser = removeUser(22)
-
-console.log(removedUser)
-console.log(users)
+module.exports = {
+    addUser,
+    removeUser,
+    getUser,
+    getUsersInRoom
+}
