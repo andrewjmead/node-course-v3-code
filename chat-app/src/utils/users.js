46e4d974c5e4a8dc3085183bcd9314778486efc2
Andrew Mead <andrewjosephmead@gmail.com>
2019-02-04 11:41:21 -0500

    Lesson: Storing Users: Part I

---
+++ chat-app/src/utils/users.js
@@ -0,0 +1,54 @@
+const users = []
+
+// addUser, removeUser, getUser, getUsersInRoom
+
+const addUser = ({ id, username, room }) => {
+    // Clean the data
+    username = username.trim().toLowerCase()
+    room = room.trim().toLowerCase()
+
+    // Validate the data
+    if (!username || !room) {
+        return {
+            error: 'Username and room are required!'
+        }
+    }
+
+    // Check for existing user
+    const existingUser = users.find((user) => {
+        return user.room === room && user.username === username
+    })
+
+    // Validate username
+    if (existingUser) {
+        return {
+            error: 'Username is in use!'
+        }
+    }
+
+    // Store user
+    const user = { id, username, room }
+    users.push(user)
+    return { user }
+}
+
+const removeUser = (id) => {
+    const index = users.findIndex((user) => user.id === id)
+
+    if (index !== -1) {
+        return users.splice(index, 1)[0]
+    }
+}
+
+addUser({
+    id: 22,
+    username: 'Andrew  ',
+    room: '  South Philly'
+})
+
+console.log(users)
+
+const removedUser = removeUser(22)
+
+console.log(removedUser)
+console.log(users)
